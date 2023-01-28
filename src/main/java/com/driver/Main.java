08ee1e732fe0572d550a63a9986ccb88a5226b45
iliyas-gangaram <iliyas7777777@gmail.com>
2023-01-27 18:31:56 -0800

    2nd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,25 @@
 package com.driver;
 
 public class Main {
-    public static void main(String arge[])
-    {
-        B b=new B();
-        b.meth();}
-}
-     class A
-    {
-        public  String meth()
-        {
+    static class A {
+        public String meth() {
             return "Invoking method from class A";
 
         }
     }
-    class B extends A
-    {
 
-        public String meth()
-        {
+    static class B extends A {
+
+        public String meth() {
 
             return "Method is overridden in Extendend class B";
 
         }
 
     }
+
+    public static void main(String args[]) {
+        B obj = new B();
+        obj.meth();
+    }
+}
